da43e0b86a92837477148ef4b8d9544162e318ee
unknown <danielortega2000@live.com>
2024-06-19 22:58:46 -0500

    Edición de clases

--- src/proyectoprimerparcial/Autor.java
+++ src/proyectoprimerparcial/Autor.java
@@ -10,8 +10,41 @@ package proyectoprimerparcial;
  */
 public class Autor extends Usuario{
     
-    public Autor(String nombre, String apellido, String correoElectronico) {
+    private String codigo;
+    private String institucion;
+    private String campoInvestigacion;
+    
+    public Autor(String nombre, String apellido, String correoElectronico, String codigo,String institucion,String campoInvestigacion) {
         super(nombre, apellido, correoElectronico);
+        this.codigo=codigo;
+        this.institucion=institucion;
+        this.campoInvestigacion=campoInvestigacion;
+    }    
+
+    
+    public String getCodigo(){
+        return codigo;
     }
     
+    public String getInstitucion(){
+        return institucion;
+    }
+    
+    public String getCampoInvestigacion(){
+        return campoInvestigacion;
+    }
+    
+    public void setCodigo(String codigo){
+        this.codigo=codigo;
+    }
+    
+    public void setInstitucion(String institucion){
+        this.institucion=institucion;
+    }
+    
+    public void setCampoInvestigacion(String campoInvestigacion){
+        this.campoInvestigacion=campoInvestigacion;
+    }
 }
+
+
